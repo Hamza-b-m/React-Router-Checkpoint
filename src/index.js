b4cc0402d22c604044031f8b40a76635b60941f1
hamza-b-m <hamza.ben.manaa@gmail.com>
2020-10-07 20:47:48 +0100

    React Router Checkpoint

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './Home';
+import Category from "./Category";
+import Products from "./Products";
+import Admin from "./Admin";
+import Navbar from "./Navbar";
+import Login from "./Login";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/category" component={Category} />
+      <Route path="/products" component={Products} />
+      <Route path="/login" component={Login} />
+      <Route path="/admin" component={Admin} />
+    </Switch>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
